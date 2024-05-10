5532d2bd15ad51899b4a4cf2be54b5bcaca1d709
Ye-eun-Kim <yk23aao@herts.ac.uk>
2024-05-11 04:43:29 +0900

    11403 not yet

--- Baekjoon/11403.py
+++ Baekjoon/11403.py
@@ -1,9 +1,27 @@
-import numpy
+import copy
 
 n = int(input())
 input_matrix = [list(map(int, input().split())) for _ in range(n)]
-result = [[0]*n for _ in range(n)]
+result = copy.deepcopy(input_matrix)
+
+
+def recur(origin, y, result):
+    for idx, value in enumerate(result[y]):
+        if value == 0:
+            continue
+        result[origin][idx] = 1
+        if origin < idx:
+            recur(origin, idx, result)
+
 
 for x, row in enumerate(input_matrix):
     for y, value in enumerate(row):
-        print(x, y, value)
+        if value == 0:
+            continue
+        result[x][y] = 1
+        recur(x, y, result)
+
+
+print(result)
+
+
