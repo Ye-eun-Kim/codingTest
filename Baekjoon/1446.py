08f766d567cf3c61426e661eacb39b3de836c8fd
Ye-eun-Kim <yk23aao@herts.ac.uk>
2024-05-15 18:05:25 +0900

    1446 finished

--- Baekjoon/1446.py
+++ Baekjoon/1446.py
@@ -1,22 +1,17 @@
-# 시작점이 정렬되어 있다는 가정 하에선 맞는 코드
+# 정답 코드이지만 다소 비효율적일지도?
 
 n, d = map(int, input().split())
+roads = [list(map(int, input().split())) for _ in range(n)]
+roads.sort(key=lambda x:x[0])
 
 graph = [i for i in range(d+1)]
 
 for i in range(n):
-    [start, end, length] = map(int, input().split())
+    [start, end, length] = roads[i]
     if end > d:
         continue
     graph[end] = min(graph[end], graph[start]+length)
     for j in range(end+1, d+1):
-        if end == 160 and j < 181:
-            print(graph[j], graph[j-1]+1)
         graph[j] = min(graph[j], graph[j-1]+1)
-        if end == 160 and j < 181:
-            print(f"결국: {graph[j]}")
-
-
-    print(f"140: {graph[140]}, 160: {graph[160]}, 180: {graph[180]}, 900: {graph[900]}")
 
 print(graph[d])
