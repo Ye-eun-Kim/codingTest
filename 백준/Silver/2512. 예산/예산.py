fd9fd43b184a777d7950c32ed3a9f0efe12599cf
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-05-19 01:33:34 +0100

    [Silver II] Title: 예산, Time: 68 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,3 +1,4 @@
+# chatGPT 코드
 n = int(input())
 requests = list(map(int, input().split()))
 requests.sort()
@@ -22,3 +23,34 @@ else:
             high = mid - 1
 
     print(result)
+
+
+# 나의 기존 코드
+
+'''
+n = int(input())
+requests = list(map(int, input().split()))
+requests.sort()
+m = int(input())
+
+sum_requests = sum(requests)
+tot_diff = sum_requests - m
+result = 0
+
+if tot_diff <= 0:
+    print(requests[-1])
+
+else:
+    for i in range(n-1, -1, -1):
+        big_num_sum = 0
+        for j in range(n-1, i-1, -1):
+            big_num_sum += requests[j]
+        temp = (big_num_sum - tot_diff) // (n-j)
+        if i == n-1:
+            result = temp
+        elif temp > result:
+            result = temp
+        else:
+            break
+    print(result)
+'''
