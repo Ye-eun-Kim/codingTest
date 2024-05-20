8d7a8bca04a653bde36e6a95cbf4566187c6b2e8
Ye-eun-Kim <yk23aao@herts.ac.uk>
2024-05-21 00:48:11 +0900

    11052 add comments

--- Baekjoon/11052.py
+++ Baekjoon/11052.py
@@ -1,3 +1,16 @@
+# 시간이 더 오래 걸리는데..? 다른 사람들 정답 코드랑 큰 차이가 없는데 대체 왜..?
+# 오히려 나는 dp만 사용해서 시간 복잡도를 줄일 수 있을 거라고 생각했었는데..
+# 공간복잡도만 줄어들고 시간 복잡도는 그대로인 것인가?
+
+# n = int(input())
+# dp = list(map(int, input().split()))
+#
+# for i in range(1, n):
+#     for j in range(i):
+#         dp[i] = max(dp[j] + dp[i - j - 1], dp[i])
+#
+# print(dp[-1])
+
 n = int(input())
 dp = list(map(int, input().split()))
 
