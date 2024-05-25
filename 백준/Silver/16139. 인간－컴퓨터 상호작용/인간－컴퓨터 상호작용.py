8e77ac6204db1ae01a8372cfb9d6855b89e9934f
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-05-25 15:52:15 +0100

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 560 ms, Memory: 193320 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,11 +1,39 @@
+import sys
+
 s = input()
 q = int(input())
-questions = [input().split() for _ in range(q)]
+questions = [sys.stdin.readline().split() for _ in range(q)]
+
+dp = {}
+chars = list(set(s))
+len_s = len(s)
+
+for j in range(len(chars)):
+    dp[chars[j]] = [0]*len_s
+
+dp[s[0]][0] = 1
+
+for i in range(1, len_s):
+    for char in chars:
+        dp[char][i] = dp[char][i-1]
+    dp[s[i]][i] += 1
 
 for question in questions:
     target, left, right = question[0], int(question[1]), int(question[2])
-    cnt = 0
-    for i in range(left, right+1):
-        if target == s[i]:
-            cnt += 1
-    print(cnt)
+    if target not in dp:
+        print(0)
+        continue
+    if left == 0:
+        if dp[target][left] == 1:
+            print(dp[target][right] - dp[target][left] + 1)
+            continue
+        print(dp[target][right] - dp[target][left])
+        continue
+    else:
+        print(dp[target][right]-dp[target][left-1])
+
+
+# for question in questions:
+#     target, left, right = question[0], int(question[1]), int(question[2])
+#     value = s[left:right+1].count(target)
+#     print(value)7-+
