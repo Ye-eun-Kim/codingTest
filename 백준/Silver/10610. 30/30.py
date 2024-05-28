7becd3a0ccdef32ced74d0466ac3ae7cfb248883
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-05-28 05:33:54 +0100

    [Silver IV] Title: 30, Time: 72 ms, Memory: 39132 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -1,21 +1,28 @@
-n = list(input())
+n = list(map(int, input()))
 
-n.sort(reverse=True)
-
-m = int("".join(n))
-
-if m % 30 == 0:
-    print(m)
-    exit()
-
-# 30의 배수라면 무조건 마지막 숫자가 0이어야 함
-if n[-1] != '0':
-    print(-1)
-    exit()
-
-tot = sum(list(map(int, n)))
-if tot % 3 == 0:
-    print(m)
+if 0 in n and sum(n) % 3 == 0:
+    n.sort(reverse=True)
+    print(''.join(str(num) for num in n))
 else:
     print(-1)
+
+
+# n.sort(reverse=True)
+#
+# m = int("".join(n))
+#
+# if m % 30 == 0:
+#     print(m)
+#     exit()
+#
+# # 30의 배수라면 무조건 마지막 숫자가 0이어야 함
+# if n[-1] != '0':
+#     print(-1)
+#     exit()
+#
+# tot = sum(list(map(int, n)))
+# if tot % 3 == 0:
+#     print(m)
+# else:
+#     print(-1)
 
