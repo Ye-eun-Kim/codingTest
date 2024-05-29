cf7b45c77980c7a24541deaa03dd508e7be7235a
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-05-29 20:15:31 +0900

    [Silver II] Title: 창고 다각형, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -4,10 +4,9 @@ infos = sorted(infos, key=lambda x: -x[1])
 right, left = infos[0][0], infos[0][0]
 node = infos.pop(0)
 r, l = node[0], node[0]
-rh, lh = node[1], node[1]
 
 # 시작 기둥의 면적 (아래 과정에서 계산되지 않음)
-area = rh
+area = node[1]
 
 while infos:
     node = infos.pop(0)
