c1d3d93a07b2fefbbb29d6bf49182027bbf6733a
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-06-25 02:01:04 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 56 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,9 +1,28 @@
+import sys, heapq
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-nums = sorted(list(map(int, input().split())))
+heap = list(map(int, input().split()))
+heapq.heapify(heap)
 
-for i in range(m):
-    temp = nums[0]+nums[1]
-    nums[0], nums[1] = temp, temp
-    nums.sort()
+for _ in range(m):
+    temp = heapq.heappop(heap) + heapq.heappop(heap)
+    heapq.heappush(heap, temp)
+    heapq.heappush(heap, temp)
 
-print(sum(nums))
+print(sum(heap))
+
+
+# heapq를 모를 때 작성한 코드(이하)
+
+# import sys
+#
+# n, m = map(int, input().split())
+# nums = sorted(list(map(int, sys.stdin.readline().split())))
+#
+# for i in range(m):
+#     temp = nums[0]+nums[1]
+#     nums[0], nums[1] = temp, temp
+#     nums.sort()
+#
+# print(sum(nums))
