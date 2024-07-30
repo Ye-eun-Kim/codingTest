b49b3d13c4400df973bf68755f1e0913ea2b5118
Ye-eun-Kim <yk23aao@herts.ac.uk>
2024-07-31 07:13:59 +0900

    1026

--- Baekjoon/1026.py
+++ Baekjoon/1026.py
@@ -2,4 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+tot = 0
+for i in range(n):
+    tot = tot + a[i] * b[i]
+
+print(tot)
 
