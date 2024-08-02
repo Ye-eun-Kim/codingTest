8e6602411cb66853e13d7599de5f237e256d298e
Ye-eun-Kim <yk23aao@herts.ac.uk>
2024-08-03 03:08:00 +0900

    013 finish

--- This is Coding Test/013.py
+++ This is Coding Test/013.py
@@ -3,16 +3,28 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input())
-coins = [input().strip() for i in range(N)]
+N, M = map(int, input().split())
+coins = [int(input().strip()) for i in range(N)]
 dp = [0]*(M+1)
 
+for coin in coins:
+    if coin < M+1:
+        dp[coin] = 1
 
 for i in range(coins[0], M+1):
     for j in range(N):
-        if i-coins[j] >= coins[0] and dp[i] != 0:
-            dp[i] = min(dp[i-coins[j]]+1,dp[i])
-        if i in coins:
-            dp[i] += 1
+        point = i+coins[j]
+        if point < M+1:
+            if dp[point] == 0:
+                dp[point] = dp[i] + 1
+            else:
+                dp[point] = min(dp[i] + 1, dp[point])
+                # min을 사용할 거니까 처음부터 1001을 사용했어도 좋았겠다~ 그러면 코드가 더 간단했을 듯.
+                # 조건문 하나 필요 없으니께
 
 
+if dp[M] != 0:
+    print(dp[M])
+
+else:
+    print(-1)
