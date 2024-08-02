0d2481a40f450e45d82acd80b12ad3dd778be2c3
Ye-eun-Kim <yk23aao@herts.ac.uk>
2024-08-03 00:00:03 +0900

    013

--- This is Coding Test/013.py
+++ This is Coding Test/013.py
@@ -1,3 +1,18 @@
 # 이것이 코딩테스트다 228p
 # 효율적인 화폐 구성
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input())
+coins = [input().strip() for i in range(N)]
+dp = [0]*(M+1)
+
+
+for i in range(coins[0], M+1):
+    for j in range(N):
+        if i-coins[j] >= coins[0] and dp[i] != 0:
+            dp[i] = min(dp[i-coins[j]]+1,dp[i])
+        if i in coins:
+            dp[i] += 1
+
 
