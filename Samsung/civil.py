1451ba956efb2e06c15930cd154df2ef7c02064b
Yeeun Kim <kimyeeun@gm.gist.ac.kr>
2024-09-30 00:33:08 +0900

    pass

--- Samsung/civil.py
+++ Samsung/civil.py
@@ -26,21 +26,25 @@ def spin(r, c, curr_graph):
         g[nr][nc] = store.pop(0)
     return g
 
-def cal(curr_graph):
+def cal(curr_graph, flag):
     val = 0
     for num in range(1, 8):
         visited = []
-        for r in range(4):
-            for c in range(4):
+        for r in range(5):
+            for c in range(5):
                 if (r, c) in visited:
                     continue
                 q = deque()
                 node = curr_graph[r][c]
+                v = []
                 if node != num:
                     continue
                 temp_val = 1
                 visited.append((r, c))
                 q.append((r, c))
+                if flag == 1:
+                    v.append((r, c))
+                    curr_graph[r][c] = 0
                 while q:
                     r, c = q.popleft()
                     for i in range(4):
@@ -51,9 +55,16 @@ def cal(curr_graph):
                             temp_val += 1
                             visited.append((nr, nc))
                             q.append((nr, nc))
+                            if flag == 1:
+                                v.append((nr, nc))
+                                curr_graph[nr][nc] = 0
                 if temp_val >= 3:
                     val += temp_val
-    return val
+                else:
+                    while v:
+                        vr, vc = v.pop(0)
+                        curr_graph[vr][vc] = num
+    return curr_graph, val
 
 def explore():
     temp_val, temp_angle, temp_center = 0, 1, (0, 0)  # temp_val이 0으로 남는다면 종료
@@ -63,40 +74,63 @@ def explore():
         t_val, t_angle = 0, 1
         curr_graph = copy.deepcopy(graph)
         for curr_angle in range(1, 4):  # angle은 1-2-3이 각각 90, 180, 270도
-            if r==1 and c==2:
-                print("now")
             curr_graph = spin(r, c, curr_graph) # 90도씩 돌리기 때문
-            curr_val = cal(curr_graph)
-            if curr_val != 0 and curr_val > temp_val:
+            curr_graph, curr_val = cal(curr_graph, 0)
+            if curr_val != 0 and curr_val > t_val:
                 t_val, t_angle = curr_val, curr_angle
         if t_val > temp_val:
-            temp_val, temp_angle, temp_center = t_val, t_angle, (r, c)
+            temp_val, temp_angle, temp_center = t_val, t_angle, curr_center
         elif t_val == temp_val and t_angle < temp_angle:
-            temp_val, temp_angle, temp_center = t_val, t_angle, (r, c)
+            temp_val, temp_angle, temp_center = t_val, t_angle, curr_center
         elif t_val == temp_val and t_angle == temp_angle:
             tr, tc = temp_center
-            if r < tr:
-                temp_val, temp_angle, temp_center = t_val, t_angle, (r, c)
-            elif r == tr and c < tc:
-                temp_val, temp_angle, temp_center = t_val, t_angle, (r, c)
+            if c < tc:
+                temp_val, temp_angle, temp_center = t_val, t_angle, curr_center
+            elif c == tc and r < tr:
+                temp_val, temp_angle, temp_center = t_val, t_angle, curr_center
     return temp_center, temp_angle, temp_val
 
+def fill(g):
+    for c in range(5):
+        if len(walls) == 0:
+            break
+        for r in range(4, -1, -1):
+            if len(walls) == 0:
+                break
+            if g[r][c] == 0:
+                g[r][c] = walls.pop(0)
+    return g
+
+def print_graph():
+    for i in range(5):
+        print(graph[i])
+    print()
 
 for k in range(K):
+    # if k == 4:
+    #     print_graph()
+    # print(k, "th")
+    # print("Before")
+    # print_graph()
+    result = 0
     center, angle, val = explore()
+    if val == 0:
+        break
+    # print(center, angle, val)
     for _ in range(angle):
         graph = spin(center[0], center[1], graph)
-    print(center, angle, val)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # print("After Spin")
+    # print_graph()
+    while val:
+        # print("1", val)
+        graph, val = cal(graph, 1)
+        # print("2", val)
+        graph = fill(graph)
+        result += val
+        # print("After fill")
+        # print_graph()
+    if result == 0:
+        continue
+    print(result, end = ' ')
+    # print()
+    # print("="*100)
